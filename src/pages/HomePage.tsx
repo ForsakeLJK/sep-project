02711a5adf23922e9b7b0d2af51b4015ab7ad6c3
forsakeljk <forsakeljk@gmail.com>
2024-10-28 13:54:21 +0100

    tried router functionality

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResponsiveAppBar from '../AppBar';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <ResponsiveAppBar />
+            <h1>Home Page</h1>
+            <p>Welcome to the homepage!</p>
+        </div>
+    );
+};
+
+export default HomePage;
