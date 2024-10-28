83a7c0163ee1b5cc1e4cc4ec57a10c6c7a53d97a
forsakeljk <forsakeljk@gmail.com>
2024-10-28 17:57:40 +0100

    add homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,96 @@
-import React from 'react';
-import ResponsiveAppBar from '../components/AppBar';
+import AdbIcon from '@mui/icons-material/Adb';
+import MailIcon from '@mui/icons-material/Mail';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import React, { useEffect, useState } from 'react';
+import { fetchData } from '../apiService';
+
+const drawerWidth = 240;
+
 
 const HomePage: React.FC = () => {
+    const [myData, setMyData] = useState(null);
+
+    useEffect(
+        () => {
+            const fetchMyData = async () => {
+                try {
+                    const response = await fetchData('home');
+                    setMyData(response.result);
+                } catch (error) {
+                    console.error('Error fetching my data:', error);
+                }
+            };
+
+            fetchMyData();
+        }, []
+    )
+
     return (
-        <div>
-            <ResponsiveAppBar />
-            <h1>Home Page</h1>
-            <p>Welcome to the homepage!</p>
-        </div>
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+                <Toolbar>
+                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        SEP
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+                }}
+            >
+                <Toolbar />
+                <Box sx={{ overflow: 'auto' }}>
+                    <List>
+                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Drawer>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <Toolbar />
+                <Typography sx={{ marginBottom: 2 }}>
+                    <h1>Hello from menu</h1>
+                </Typography>
+            </Box>
+        </Box>
     );
 };
 
